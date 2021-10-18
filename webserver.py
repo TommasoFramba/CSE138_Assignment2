c420e01d8f8830eeb1f6c50f2d906c2510fff109
Tommaso Marco Framba <frambatom@gmail.com>
2021-10-17 23:24:04 -0700

    second commit part 1 passes 10/10 tests

--- webserver.py
+++ webserver.py
@@ -1,10 +1,11 @@
 ####################
 # Course: CSE138
 # Date: Fall 2021
-# Assignment: 1
+# Assignment: 2
 # Tommaso Framba
+# Eric Yao Huang
 # This program implements a simple HTTP interface with specified
-# responses to GET and POST requests.
+# responses to GET and PUT and DELETE requests.
 ###################
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -12,9 +13,6 @@ from urllib.parse import urlparse
 import os.path
 import json
 from html.parser import HTMLParser
-
-
-
 
 #handle requests
 class helloHandler(BaseHTTPRequestHandler):
@@ -29,14 +27,18 @@ class helloHandler(BaseHTTPRequestHandler):
                 #If key exists #200 OK else #404 Not Found
                 if parsed_path[2] in self.keyValueStore:
                     self.send_response(200)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write(('{"result": "found", "value": "' + str(self.keyValueStore[parsed_path[2]]) + '"}').encode("utf8"))
+                    jsndict = { "result": "found", "value": self.keyValueStore[parsed_path[2]] }
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
                 else:
                     self.send_response(404)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write((r"""{"error": "Key does not exist"}""""").encode("utf8"))
+                    jsndict = {"error": "Key does not exist"}
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
 
     #handle post requests
     def do_PUT(self):
@@ -52,29 +54,37 @@ class helloHandler(BaseHTTPRequestHandler):
                 #400 BAD REQUEST KEY TOO LONG
                 if len(parsed_path[2]) > 50:
                     self.send_response(400)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write((r"""{"error": "Key is too long"}""""").encode("utf8"))
+                    jsndict = {"error": "Key is too long"}
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
                 #400 BAD REQUEST NO VALUE SPECIFIED
                 elif 'value' not in data:
                     self.send_response(400)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write((r"""{"error": "PUT request does not specify a value"}""""").encode("utf8"))
+                    jsndict = {"error": "PUT request does not specify a value"}
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
                 elif 'value' in data:
                     ##200 OK
                     if parsed_path[2] in self.keyValueStore:
                         self.keyValueStore[parsed_path[2]] = data['value']
                         self.send_response(200)
-                        self.send_header("Content-type", "text/html")
+                        self.send_header("Content-type", "application/json")
                         self.end_headers()
-                        self.wfile.write((r"""{"result": "replaced"}""""").encode("utf8"))
+                        jsndict = {"result": "replaced"}
+                        jsnrtrn = json.dumps(jsndict)
+                        self.wfile.write(jsnrtrn.encode("utf8"))
                     ##201 CREATED
                     else:
                         self.send_response(201)
-                        self.send_header("Content-type", "text/html")
+                        self.send_header("Content-type", "application/json")
                         self.end_headers()
-                        self.wfile.write((r"""{"result": "created"}""""").encode("utf8"))
+                        jsndict = {"result": "created"}
+                        jsnrtrn = json.dumps(jsndict)
+                        self.wfile.write(jsnrtrn.encode("utf8"))
                         self.keyValueStore[parsed_path[2]] = data['value']
 
     def do_DELETE(self):
@@ -84,14 +94,18 @@ class helloHandler(BaseHTTPRequestHandler):
                 if parsed_path[2] in self.keyValueStore:
                     del self.keyValueStore[parsed_path[2]]
                     self.send_response(200)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write((r"""{"result": "deleted"}""""").encode("utf8"))
+                    jsndict = {"result": "deleted"}
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
                 else:
                     self.send_response(404)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "application/json")
                     self.end_headers()
-                    self.wfile.write((r"""{"error": "Key does not exist"}""""").encode("utf8"))
+                    jsndict = {"error": "Key does not exist"}
+                    jsnrtrn = json.dumps(jsndict)
+                    self.wfile.write(jsnrtrn.encode("utf8"))
 
 
 #start and run server on port 8090
